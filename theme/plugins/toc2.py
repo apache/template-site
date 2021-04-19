b0fa3f5d13e562d46c8cbe6c05a64fbc4b6f5822
Dave Fisher <dave@davefisher.tech>
2021-04-19 15:51:56 -0700

    slugify like current CMS

--- theme/plugins/toc2.py
+++ theme/plugins/toc2.py
@@ -71,7 +71,9 @@ class HtmlTreeNode(object):
             new_string = "".join(new_string)
 
         if not new_id:
-            new_id = slugify(new_string, character_map)
+            new_slug = new_string
+            new_slug.translate(character_map)
+            new_id = slugify(new_slug)
 
         new_id = unique(new_id, ids)  # make sure id is unique
 
