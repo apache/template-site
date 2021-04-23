0c9f4b5d4b4015c5263e4eb6ac3454f1afca91f8
Dave Fisher <dave@davefisher.tech>
2021-04-23 10:13:55 -0700

    Ask for jinja2content plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -82,7 +82,7 @@ AUTHOR_FEED_RSS = None
 # Pelican Plugins
 # The provided location. If the buildbot does not have a new plugin then look into requirements.txt
 PLUGIN_PATHS = ['./theme/plugins']
-PLUGINS = ['genid', 'pelican-jinja2content', 'pelican-gfm', 'sitemap']
+PLUGINS = ['genid', 'jinja2content', 'pelican-gfm', 'sitemap']
 
 GENID = {
     'elements': True,
