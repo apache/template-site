a9a3fcf7ea584251e6428747ba27a25ab76b2eb8
Dave Fisher <dave2wave@comcast.net>
2021-04-26 10:24:10 -0700

    Create asfreader.py

---
+++ theme/plugins/asfreader.py
@@ -0,0 +1,41 @@
+"""
+asfreader.py
+----------------
+Pelican plugin that processes Markdown files through as an ezt template then through GitHub Flavored Markdown.
+"""
+
+import os
+from tempfile import NamedTemporaryFile
+
+from gfm import GFMReader
+
+from pelican import signals
+from pelican.utils import pelican_open
+
+
+class ASFReadertMixin:
+    def __init__(self, *args, **kwargs):
+        # super().__init__(*args, **kwargs)
+
+        # look for the ASFDATA yaml file
+        print("ASFReaderMixin.__init__: %s" % self.settings("ASFDATA"))
+        # read the ASFDATA yaml file
+        # slurp the datasources
+        # flatten for ezt
+
+    def read(self, source_path):
+        print("ASFReaderMixin.read: %s" % source_path)
+        content, metadata = super().read(source_path)
+        return content, metadata
+
+
+class ASFReader(ASFReaderMixin, GFMReader):
+    pass
+
+
+def add_reader(readers):
+    readers.reader_classes['emd'] = ASFReader
+
+
+def register():
+    signals.readers_init.connect(add_reader)
