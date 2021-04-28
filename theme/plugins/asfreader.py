702564afb7520a4e794742cb5573ac26d6f4f148
Dave Fisher <dave@davefisher.tech>
2021-04-28 08:44:23 -0700

    Cleanup ASFReader - AL license

--- theme/plugins/asfreader.py
+++ theme/plugins/asfreader.py
@@ -1,8 +1,24 @@
-"""
-asfreader.py
-----------------
-Pelican plugin that processes Markdown files through as an ezt template then through GitHub Flavored Markdown.
-"""
+#!/usr/bin/python -B
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+#
+# asfreader.py -- Pelican plugin that processes ezt template Markdown through ezt and  then GitHub Flavored Markdown.
+#
 
 import os.path
 import sys
@@ -17,6 +33,9 @@ GFMReader = sys.modules['pelican-gfm.gfm'].GFMReader
 
 class ASFReader(GFMReader):
     enabled = True
+    """GFM-flavored Reader for the Pelican system that adds ASF data and ezt 
+    generation prior to processing the GFM
+    """
     file_extensions = ['ezmd', 'emd']
 
     def read(self, source_path):
@@ -47,6 +66,7 @@ class ASFReader(GFMReader):
         return content, metadata
 
 
+# The following are required or ezmd files are not processed
 def add_readers(readers):
     readers.reader_classes['ezmd'] = ASFReader
 
