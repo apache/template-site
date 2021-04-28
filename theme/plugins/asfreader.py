d8045f9fd81051f45d167813293b08b7aabc7874
Dave Fisher <dave@davefisher.tech>
2021-04-28 07:39:58 -0700

    Remove read_sources from ASFReader. Comment out signal

--- theme/plugins/asfreader.py
+++ theme/plugins/asfreader.py
@@ -22,61 +22,6 @@ class ASFReader(GFMReader):
     enabled = True
     file_extensions = ['ezmd', 'emd']
 
-    # Note: name starts in column 0, no whitespace before colon, will be
-    #       made lower-case, and value will be stripped
-    #
-    RE_METADATA = re.compile('^([A-za-z]+): (.*)$')
-
-    def read_source(self, source_path):
-        "Read metadata and content from the source."
-
-        # Prepare the "slug", which is the target file name. It will be the
-        # same as the source file, minus the leading ".../content/(articles|pages)"
-        # and with the extension removed (Pelican will add .html)
-        relpath = os.path.relpath(source_path, self.settings['PATH'])
-        parts = relpath.split(os.sep)
-        parts[-1] = os.path.splitext(parts[-1])[0]  # split off ext, keep base
-        slug = os.sep.join(parts[1:])
-
-        metadata = {
-            'slug': slug,
-        }
-        # Fetch the source content, with a few appropriate tweaks
-        with pelican.utils.pelican_open(source_path) as text:
-
-            # Extract the metadata from the header of the text
-            lines = text.splitlines()
-            for i in range(len(lines)):
-                line = lines[i]
-                match = ASFReader.RE_METADATA.match(line)
-                if match:
-                    name = match.group(1).strip().lower()
-                    if name != 'slug':
-                        value = match.group(2).strip()
-                        if name == 'date':
-                           value = pelican.utils.get_date(value)
-                    metadata[name] = value
-                    #if name != 'title':
-                    #  print 'META:', name, value
-                elif not line.strip():
-                    # blank line
-                    continue
-                else:
-                    # reached actual content
-                    break
-
-            # Redo the slug for articles.
-            # depending on pelicanconf.py this will change the output filename
-            if parts[0] == 'articles' and 'title' in metadata:
-                metadata['slug'] = pelican.utils.slugify(
-                    metadata['title'],
-                    self.settings.get('SLUG_SUBSTITUTIONS', ()))
-
-            # Reassemble content, minus the metadata
-            text = '\n'.join(lines[i:])
-
-            return text, metadata
-
     def read(self, source_path):
         "Read metadata and content, process content as ezt template, then render into HTML."
 
@@ -111,9 +56,9 @@ class ASFReader(GFMReader):
         return content, metadata
 
 
-def add_readers(readers):
-    readers.reader_classes['ezmd'] = ASFReader
+# def add_readers(readers):
+#    readers.reader_classes['ezmd'] = ASFReader
 
 
-def register():
-    pelican.plugins.signals.readers_init.connect(add_readers)
+# def register():
+#    pelican.plugins.signals.readers_init.connect(add_readers)
