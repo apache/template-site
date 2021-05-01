6348fdd6c983038ea706a7bd43caf5c6686e2026
Dave Fisher <dave@davefisher.tech>
2021-05-01 12:46:17 -0700

    debug split loaded dict into component dicts

--- theme/plugins/asfdata.py
+++ theme/plugins/asfdata.py
@@ -59,7 +59,7 @@ def url_data(url):
 def load_splits(metadata, value, key, load):
     if 'splits' in value:
         for split in value['splits']:
-            ref = split[0]
+            ref = value['splits'][split][0]
             print(f"{split} {ref}")
             reference = load[ref]
             metadata[split] = reference
