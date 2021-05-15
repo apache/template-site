9bb7df9fc831aa48d4ae886de1e72aa410a662a1
Dave Fisher <dave@davefisher.tech>
2021-05-15 09:23:39 -0700

    introduce simple bug

--- theme/plugins/asfdata.py
+++ theme/plugins/asfdata.py
@@ -238,7 +238,7 @@ def process_sequence(metadata, seq, sequence, load, debug):
         if debug:
             print(f"random: {sequence['random']}")
         if is_sequence:
-            reference = random.sample(reference, sequence['random'])
+            reference = random.stample(reference, sequence['random'])
         else:
             print(f"{seq} - random requires an existing sequence to sample")
 
